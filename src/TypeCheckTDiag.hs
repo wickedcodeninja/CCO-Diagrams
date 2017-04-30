44feddee05127997ae40b7b66c3425ecd41446dd
wickedcodeninja <wicked@codeninja>
2017-05-01 00:29:41 +0200

    Implement first part of the type checker.

--- src/TypeCheckTDiag.hs
+++ src/TypeCheckTDiag.hs
@@ -1,10 +1,20 @@
+-- (C) 2017 Wout Elsinghorst
+
 import CCO.Component  (Component, component, printer, ioWrap)
 import CCO.Diag       (Diag)
 import CCO.Tree       (ATerm, Tree (fromTree, toTree), parser)
 import Control.Arrow  (Arrow (arr), (>>>))
 
+import CCO.Feedback ( errorMessage )
+import CCO.Printing ( text )
+
+import CCO.Diag.AG ( DiagramType, diagramType, isTypeCorrect )
+
 typeCheckDiag :: Component Diag Diag
-typeCheckDiag = arr id
+typeCheckDiag = component $ \a -> 
+  case isTypeCorrect a of
+    Right tree -> pure tree
+    Left  err  -> errorMessage $ text err
 
 main = 
   ioWrap (parser >>> (component toTree :: Component ATerm Diag) >>> typeCheckDiag >>> arr fromTree >>> printer)
